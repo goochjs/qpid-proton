116ab4b9cdb2b044dbe04a97994ee02957b25794
Alan Conway <aconway@redhat.com>
2016-09-01 12:13:44 -0400

    WIP: no callbacks in container

--- proton-c/include/proton/container.h
+++ proton-c/include/proton/container.h
@@ -21,22 +21,28 @@
  */
 
 /**@file
+ *
  * Container API for the proton engine.
+ *
  * @defgroup container Container
  * A container for AMQP connections.
  *
- * The container manages multiple connections and dispatches events to handler
- * functions in one or more threads. If you call pn_container_run() in a single
- * thread and don't have any other threads then your application is single
- * threaded.
+ * The container manages IO for multiple connections and dispatches pn_event_t
+ * events to threads that call pn_container_wait(). It can operate in single or
+ * multi-threaded mode. See pn_container_wait() for details.
  *
- * In a multi-threaded application, most pn_container_() functions are thread
- * safe but always read the *Thread Safety* notes. The @ref engine functions
- * and pn_incref(), pn_decref() are *not* thread safe when operating on values
- * belonging to a single connection. They are safe to call concurrently on
- * values belonging to separate connections. pn_container_inject() allows you
- * to safely "inject" a function call into the proper event-loop thread for a
- * connection.
+ * The container can make connections as a client and/or listen for connections
+ * as a server. Container functions are asynchronous: they do not return error
+ * values but cause events to be dispatched which may contain error values.
+ *
+ * The container API is designed to be usable in C, but is as simple as possible
+ * to make multiple implementations easier. It does not use any call-back
+ * functions to avoid difficulties with language binding tools like SWIG, CGO or
+ * CFFI, as well as deadlock and other re-entrancy problems in multi-threaded
+ * code.
+ *
+ * External libraries can provide alternate container implementations which may
+ * or may not support multi-threading. See @io_integration.
  *
  * @{
  */
@@ -48,117 +54,179 @@
 extern "C" {
 #endif
 
-// FIXME aconway 2016-08-16: thread safety review & doc.
-// use incref in handlers or before connnect() to pin connections.
-
-// FIXME: This API designed to be as simple as possible to implement while supporting
-// all the needs of language binding containers like C++, Python, Ruby, Go.
-// Extra conveniences can be added in the bindings, or later in C if we find they
-// are useful in many languages.
-
-// Forward declarations
 typedef struct pn_connection_t pn_connection_t;
 typedef struct pn_event_t pn_event_t;
 
-/// The container struct. Not a reference-counted object, do not use pn_incref()/pn_decref().
+/**
+ * The container struct.
+ *
+ * Use pn_container_free() to free, this is *not* a reference-counted object, do
+ * *not* use pn_incref()/pn_decref().
+ */
 typedef struct pn_container_t pn_container_t;
 
-/// Create a default container. Must be freed with pn_container_free.
-/// Not a reference-counted object, do not use pn_incref()/pn_decref().
-PN_EXTERN pn_container_t* pn_container(const char* id);
+/**
+ * Create a container, free with pn_container_free().
+ * pn_container_t is NOT a reference-counted object, do not use pn_incref()/pn_decref().
+ */
+PN_EXTERN pn_container_t *pn_container(const char *id);
 
-/// Free the container.
-PN_EXTERN void pn_container_free(pn_container_t* c);
+/** Free the container. */
+PN_EXTERN void pn_container_free(pn_container_t *c);
 
-/// Get the container ID. The string is valid until the container is freed.
-PN_EXTERN const char* pn_container_id(pn_container_t* c);
+/**
+ * Create a client connection to a remote address string (see @ref url).
+ *
+ * Dispatch a PN_CONNECTION_INIT event with the un-opened connection.
+ *
+ * When the event is returned by pn_connection_wait(), check for connect errors
+ * on the pn_connection_condition(). If there are none, configure the connection
+ * and call pn_connection_open() to start the AMQP protocol.
+ *
+ * @param context passed with the PN_CONNECTION_INIT event as pn_event_context()
+ */
+PN_EXTERN void pn_container_connect(pn_container_t *c, const char *url, void *context);
 
-/// Handler function pointer, called by the container to handle an @ref event.
-typedef void (*pn_handler_fn)(pn_event_t* event);
+/**
+ * Listen for incoming connections on URL.
+ *
+ * When a connection is accepted, dispatch a PN_CONNECTION_INIT event
+ * containing the un-opened connection.
+ *
+ * When the event is retunred by pn_connection_wait() you can reject the
+ * connection by setting an error on pn_connection_condition(). Otherwise event
+ * dispatch for the connection will continue with PN_CONNECTION_REMOTE_OPEN etc.
+ *
+ * If a listener fails a PN_CONTAINER_LISTENER_ERROR event with the context
+ * will be dispatched.
+ *
+ * @param context passed with the PN_CONNECTION_INIT event as pn_event_context()
+ */
+PN_EXTERN void pn_container_listen(pn_container_t *c, const char *url, void *context);
 
-/// Connect to a remote address string (see @ref url). The returned connection
-/// is not active, so you can safely configure settings before calling
-/// pn_container_activate().
-///
-/// @return connection, belongs to the container. Connection errors errors will
-/// be reported to the connection's handler when you call pn_container_activate().
-PN_EXTERN pn_connection_t* pn_container_connect(pn_container_t* c, const char* url);
+// FIXME aconway 2016-09-01: Need a place to attach error condition for
+// PN_CONTAINER_LISTENER_ERROR. There's no connection or transport and the
+// container is shared. Would be nice to attach to the event itself?
 
-/// Call pn_connection_open() and activate the connection so that the container
-/// will call handler to receive events related to the connection.
-///
-/// *Thread safety*: the connection's handler is active after this, use
-/// pn_container_inject to inject operations from another thread.
-PN_EXTERN void pn_container_activate(pn_container_t* c, pn_connection_t*, pn_handler_fn handler);
+// FIXME aconway 2016-09-01: review/document incoming events for security.
+// We start at CONN_INIT since that gives us visibility & influence on the entire lifecycle.
+// but we may want to document use of INIT, BOUND, OPEN, TRANSPORT_AUTHENTICATED, others?
+//
+// Can we keep all the connection central and avoid transport events or do we
+// need need to look at the transport before it has a connection?
 
-/// Accept function pointer, called when an incoming connection is accepted by
-/// the container.
-///
-/// The accept function can reject the connection by setting
-/// pn_connection_condition() and call pn_connection_close(), or accept it by
-/// calling pn_container_activate().
-///
-/// @param c the connection, NULL means listener is closed, there will be no more calls.
-/// @param error message if listener closed due to error, NULL otherwise.
-/// @param context pointer provided by the application in listen.
-typedef void (*pn_accept_fn)(pn_connection_t* c, const char* error, void* context);
+/**
+ * Close the listener(s) identified by the context.
+ *
+ * Dispatch a PN_CONTAINER_LISTENER_CLOSED event for each listener closed.
+ *
+ * @param context identifies listener(s) to close and is passed to pn_event_context()
+ */
+PN_EXTERN void pn_container_close_listener(pn_container_t *c, void *context);
 
-/// Opaque identifier for a stream of incoming connections started by pn_listen.
-typedef struct pn_listener_t pn_listener_t;
+/**
+ * Close all open listeners.
+ *
+ * Dispatche a PN_CONTAINER_LISTENER_CLOSED event for each listener, with the listener's context.
+ *
+ * @param context identifies listener(s) to close.
+ */
+PN_EXTERN void pn_container_close_listeners(pn_container_t *c);
 
-/// Listen on URL, call accept(connection, context) for each incoming connection.
-PN_EXTERN pn_listener_t* pn_container_listen(
-    pn_container_t* c, const char* url, pn_accept_fn accept, void* context);
-
-/// Stop listening
-PN_EXTERN void pn_container_stop_listening(pn_container_t* c, pn_listener_t* listener);
-
+/** Timeout value indicating wait forever */
 #define PN_FOREVER -1
 
-/// work() does some container-defined unit of work then returns.
-///
-/// *Thread Safety*: May be called in multiple threads.
-///
-/// timeout==0 means return immediately if there is no work to do.
-/// timeout>0 means return after timeout ms if there's no work.
-/// timeout==PN_FOREVER means return only if pn_container_interrupt is called.
-///
-/// @return
-/// - PN_OK: Some work was done.
-/// - PN_TIMEOUT: the timeout expired with no work done.
-/// - PN_INTR: pn_container_interrupt was called.
-/// - PN_EOS: pn_container_stop was called.
-///
-PN_EXTERN int pn_container_work(pn_container_t* c, pn_nanoseconds_t timeout);
+/**
+ * Wait for a dispatched event to be available.
+ *
+ * Once the event is processed, you *must* call pn_event_done(event)
+ *
+ * A single-threaded application calls pn_container_wait() in a loop until
+ * it returns a PN_CONTAINER_STOPPED event.
+ *
+ * *Thread Safety*: A multi-threaded application can call pn_container_wait() in
+ * multiple threads to create a thread pool. Different connections can be
+ * processed concurrently in different threads.
+ *
+ * Note however that the @ref engine API types and pn_incref()/pn_decref() are
+ * *not* thread safe for values associated with a *single* connection.
+ * pn_container_wait() ensures that events for a single connection are never
+ * returned concurrently so you can safely use the returned event without
+ * locking. You must call pn_event_done() when you are finished so further
+ * events for the same connection can be dispatched.
+ *
+ * Sometimes threads other than the pn_container_wait() thread need to affect a
+ * connection. pn_container_inject() injects a custom event that is dispatched
+ * to a pn_container_wait() thread where the connection can be used safely.
+ */
+PN_EXTERN pn_event_t *pn_container_wait(pn_container_t *c, pn_nanoseconds_t timeout);
 
-/// Run till container is stopped.
-/// Equivalent to `while (pn_container_work(c, PN_FOREVER) != PN_EOS);`
-PN_EXTERN void pn_container_run(pn_container_t* c);
 
-/// Make work() return immediately in all threads with PN_INTR
-PN_EXTERN void pn_container_interrupt(pn_container_t* c);
+/**
+ * Indicate that we are done processing this event.
+ */
+PN_EXTERN pn_event_t *pn_event_done(pn_event_t *e);
+
+/* FIXME aconway 2016-09-01: move to event.h? */
+
+/**
+ * Interrupt one pn_container_wait() thread.
+ *
+ * Returns PN_CONTAINER_INTERRUPT event immediately in *at most one* thread
+ * blocked in pn_container_wait().  If no threads are currently blocked, this
+ * does nothing. Allows shrinking the thread pool as well as growing it.
+ *
+ * *Thread safe*
+ *
+ * @param context passed with the PN_CONTAINER_INTERRUPT event as pn_event_context()
+ */
+PN_EXTERN void pn_container_interrupt(pn_container_t *c, void *context);
 
 
-/// Mark the container as stopped, abort all connections, clean up all connection memory.
-/// pn_container_work() returns in all threads with PN_EOS, future calls to work() return PN_EOS immediately.
-PN_EXTERN void pn_container_stop(pn_container_t* c);
+/**
+ * Schedule a timed event.
+ *
+ * Dispatch a PN_CONTAINER_TIMER event after delay.
+ *
+ * *Thread safe*
+ *
+ * @param context passed with the PN_CONNECTION_TIMER event as pn_event_context()
+ */
+PN_EXTERN void pn_container_schedule(pn_container_t *c, pn_nanoseconds_t delay, void *context);
 
-/// A simple callback function with void* context.
-typedef void (*pn_callback_fn)(void* context);
+/**
+ * Inject a custom event.
+ *
+ * Dispatch a PN_CONTAINER_INJECT event, serialized with other events for the
+ * connection.  When pn_container_wait() returns the event, it is safe to use
+ * the connection as no other thread is using it. You must call pn_event_done()
+ * when you are finished to allow dispatch to continue for that connection.
+ *
+ * *Thread safe*.
+ *
+ * @param context passed with the PN_CONNECTION_STOPPED event as pn_event_context()
+ */
+PN_EXTERN void pn_container_inject(pn_container_t *c, pn_connection_t *connection, void *context) ;
 
-/// Arrange for callback(context) to be executed after a delay
-///
-/// *Thread safety*: may be executed concurrently.
-PN_EXTERN void pn_container_schedule(
-    pn_container_t* c, pn_nanoseconds_t, pn_callback_fn callback, void* context);
+/**
+ * Equivalent to calling pn_container_inject() for all currently existing connections.
+ * Can be used to implement global shut-down or similar operations.
+ *
+ * Note: if there are still open listeners, or threads calling pn_container_connect() then
+ * connections created concurrently or after this call may not be included.
+ */
+PN_EXTERN void pn_container_inject_all(pn_container_t *c, void *context) ;
 
-/// Arrange for callback(context) to be executed as soon as possible in the event-loop
-/// associated with connection.
-///
-/// *Thread safety*: may be executed concurrently.  Will be serialized with
-/// calls to the event handler for that connection.
-PN_EXTERN void pn_container_inject(
-    pn_container_t* c, pn_connection_t* connection, pn_callback_fn callback, void* context) ;
+/*
+ * FIXME aconway 2016-09-01: note there is no stop(). It can be implemented with
+ * close_listeners, inject_all and interrupt, with any desired semantics
+ * (abrupt, send close(), wait for close() (up to timeout) etc.) The desired
+ * semantics of stop() are still to vague to pin down in C but we can implement
+ * the semantics of existing containers (C++, python etc.) using these tools.
+ */
+
+/* Get the container ID. The string is valid until the container is freed. */
+PN_EXTERN const char *pn_container_id(pn_container_t *c);
 
 /// Get the container associated with a connection.
 PN_EXTERN pn_container_t *pn_connection_container(pn_connection_t*);
